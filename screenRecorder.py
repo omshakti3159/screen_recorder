281c6273907f98fd0a1c166ec1329b838c9dd5ec
omshakti3159 <mauryaomshakti@gmail.com>
2021-05-11 18:03:30 +0530

    second commit

--- screenRecorder.py
+++ screenRecorder.py
@@ -10,7 +10,6 @@ time_stamp=datetime.datetime.now().strftime('%Y-%M-%D-%H-%M-%S')
 file_name = f'{time_stamp}.mp4'
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
 captured_video=cv2.VideoWriter('output.mp4',fourcc,10.0,(width,height))
-
 while True:
     img = ImageGrab.grab(bbox=(0,0,width,height))
     img_np =np.array(img)
